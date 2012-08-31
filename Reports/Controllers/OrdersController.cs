0395d66b5fcc7343bbe5c509d1208913fe7ffa21
Steve Saporta <ssaporta@localupsolutions.com>
2012-08-31 17:01:08 -0400

    Massive progress on order lookup

--- Reports/Controllers/OrdersController.cs
+++ Reports/Controllers/OrdersController.cs
@@ -10,6 +10,8 @@ namespace Reports.Controllers
 {
     public class OrdersController : Controller
     {
+        string connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["OOSDatabase"].ConnectionString;
+
         public ActionResult Index()
         {
             return View();
@@ -23,8 +25,7 @@ namespace Reports.Controllers
 
         public ActionResult LookupResults(Reports.Models.OrderParamsModel orderParams)
         {
-            string connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["OOSDatabase"].ConnectionString;
-            string query = "select top 100 * from AllOrders o join Customers cu on cu.CustomerID = o.CustomerID join Restaurants r on r.RestaurantID = o.RestaurantID join Campuses c on c.CampusID = r.CampusID where";
+            string query = "select top 100 o.OrderID, o.PaymentMethod, o.OrderPlacedDate, o.OrderTotal, o.OrderStatus, case o.IsTakeout when 1 then 'Takeout' else 'Delivery' end as OrderType, cu.FName, cu.LName, cu.LoginEmail, c.Name as CampusName, r.Name as RestaurantName from AllOrders o join Customers cu on cu.CustomerID = o.CustomerID join Restaurants r on r.RestaurantID = o.RestaurantID join Campuses c on c.CampusID = r.CampusID where";
             bool firstClause = true;
             if (orderParams.OrderID != null && orderParams.OrderID.Trim().Length > 0)
             {
@@ -50,6 +51,7 @@ namespace Reports.Controllers
                 query += (firstClause ? "" : " and") + " cu.LName like '" + orderParams.LastName.Trim() + "%'";
                 firstClause = false;
             }
+            query += " order by OrderID desc";
 
             List<Reports.Models.OrderModel> orders = new List<Models.OrderModel>();
             if (!firstClause)
@@ -61,6 +63,30 @@ namespace Reports.Controllers
                 }
             }
             return View(orders);
+        }
+
+        public ActionResult LookupDetails(int orderID)
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                string query = "select a.Name as AffiliateName, c.Name as CampusName, o.CartTotal, o.CCAddressOnCard, o.CCCityOnCard";
+                query += ", o.CCExpire, o.CCNameOnCard, o.CCStateOnCard, o.CCType, o.CCZipOnCard, o.CustomerID";
+                query += ", c.Phone as CustomerPhone, o.DeliveryAddr1, o.DeliveryAddr2, o.DeliveryCity, o.DeliveryState";
+                query += ", o.DeliveryZip, o.DeliveryFee, o.FirstOrder, o.FName, o.IsLUCCRestaurant, o.IsMobileOrder, o.LName";
+                query += ", cu.LoginEmail, o.OrderID, o.OrderPlacedDate, o.OrderStatus, o.OrderTotal";
+                query += ", case o.IsTakeout when 1 then 'Takeout' else 'Delivery' end as OrderType";
+                query += ", o.PaymentMethod, p.PartnerName, o.ProcessingFee, o.RestaurantID, r.Name as RestaurantName";
+                query += ", r.Phone1 as RestaurantPhone, o.SpecialIntructions"; // sic
+                query += ", o.Tax, o.Tip";
+                query += " from AllOrders o join Customers cu on cu.CustomerID = o.CustomerID";
+                query += " join Restaurants r on r.RestaurantID = o.RestaurantID join Campuses c on c.CampusID = r.CampusID";
+                query += " join Partners p on p.PartnerID = o.PartnerID";
+                query += " left outer join Affiliates a on a.AffiliateID = o.AffiliateID";
+                query += " where o.OrderID = " + orderID;
+                Reports.Models.OrderModel order = conn.Query<Reports.Models.OrderModel>(query).ToList().First();
+                return View(order);
+            }
         }
 
         public ActionResult Reports()
--- Reports/Models/OrderModels.cs
+++ Reports/Models/OrderModels.cs
@@ -8,18 +8,51 @@ namespace Reports.Models
 {
     public class OrderParamsModel
     {
-        public string OrderID { get; set; }
         public string Date { get; set; }
-        public string LoginEmail { get; set; }
+        public string OrderID { get; set; }
         public string LastName { get; set; }
+        public string LoginEmail { get; set; }
     }
 
     public class OrderModel
     {
-        public int OrderID { get; set; }
-        public double OrderTotal { get; set; }
-        public string PaymentMethod { get; set; }
+        public string AffiliateName { get; set; }
+        public string CampusName { get; set; }
+        public double CartTotal { get; set; }
+        public string CCAddressOnCard { get; set; }
+        public string CCCityOnCard { get; set; }
+        public string CCExpire { get; set; }
+        public string CCNameOnCard { get; set; }
+        public string CCStateOnCard { get; set; }
+        public string CCType { get; set; }
+        public string CCZipOnCard { get; set; }
+        public int CustomerID { get; set; }
+        public string CustomerPhone { get; set; }
+        public string DeliveryAddr1 { get; set; }
+        public string DeliveryAddr2 { get; set; }
+        public string DeliveryCity { get; set; }
+        public string DeliveryState { get; set; }
+        public string DeliveryZip { get; set; }
+        public double DeliveryFee { get; set; }
+        public bool FirstOrder { get; set; }
         public string FName { get; set; }
+        public bool IsLUCCRestaurant { get; set; }
+        public bool IsMobileOrder { get; set; }
         public string LName { get; set; }
+        public string LoginEmail { get; set; }
+        public int OrderID { get; set; }
+        public DateTime OrderPlacedDate { get; set; }
+        public string OrderStatus { get; set; }
+        public double OrderTotal { get; set; }
+        public string OrderType { get; set; }
+        public string PaymentMethod { get; set; }
+        public string PartnerName { get; set; }
+        public double ProcessingFee { get; set; }
+        public int RestaurantID { get; set; }
+        public string RestaurantName { get; set; }
+        public string RestaurantPhone { get; set; }
+        public string SpecialInstructions { get; set; }
+        public double Tax { get; set; }
+        public double Tip { get; set; }
     }
 }
